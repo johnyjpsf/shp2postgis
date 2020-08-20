1d0310771faa19ae4d910725f51dcfda10f27a9b
johny5 <johnypatrick5@gmail.com>
2020-08-20 20:42:29 -0300

    criando arquivos para distribuição

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="johnyjpsf",
+    version="0.0.1",
+    author="Johny Patrick da Silva Ferreira",
+    author_email="johnypatrick5@gmail.com",
+    description="shapefile to sql converter.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnyjpsf/shp2posgis",
+    packages=setuptools.find_packages(),
+    keywords='gis geospatial geographic shapefile shapefiles postgis postgre sql database',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.6'
+)
