c13ed0b145df23d3cb5e7737549395357e074209
johny5 <johnypatrick5@gmail.com>
2020-10-20 22:17:57 -0300

    versão 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="shp2postgis",
-    version="0.1.2",
+    version="0.1.4",
     author="Johny Patrick da Silva Ferreira",
     author_email="johnypatrick5@gmail.com",
     description="shapefile to sql converter.",
